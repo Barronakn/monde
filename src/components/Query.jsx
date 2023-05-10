0e9a4ed0469e3980a5818ada666caaa4ff3f68be
Barron <barronakn@gmail.com>
2023-05-10 01:23:42 +0100

    Mise à jour

--- src/components/Query.jsx
+++ src/components/Query.jsx
@@ -4,9 +4,10 @@ import { useState, useEffect } from "react";
 import "simple-reveal/index.css";
 import ArrowTop from "../assets/arrow-top.svg";
 import ArrowDown from "../assets/down-arrow.svg";
-import { document } from "postcss";
 
 const Query = () => {
+  const [handleClick, setHandleClick] = useState(false);
+
   //ToogleContinent
   const [toogle, setToogle] = useState(false);
 
@@ -58,31 +59,6 @@ const Query = () => {
       return data;
     }
   );
-
-  //ScrollFiltre
-
-  // const [scrollPosition, setScrollPosition] = useState(0);
-
-  // const handleScrollFiltre = () => {
-  //   const position = window.scrollY;
-
-  //   setScrollPosition(position);
-  // };
-
-  // useEffect(() => {
-  //   window.addEventListener("scroll", handleScrollFiltre);
-
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScrollFiltre);
-  //   };
-  // }, []);
-
-  // let top = document.querySelector("section");
-  // console.log(top);
-  // const filterTop = top.classList.add("top");
-  // const navbarStyle = {
-  //   display: scrollPosition > 0 ? filterTop : "",
-  // };
 
   //Infinite scroll
   const perPage = 30;
@@ -155,12 +131,171 @@ const Query = () => {
       <h1 className="text-center bg-white w-full fixed font-bold p-10 text-4xl animate z-50">
         Liste des pays du monde
       </h1>
-
-      <div className="cadre flex relative">
-        <section
-          // style={navbarStyle}
-          className="filter py-4 left-3 top-32 w-72 fixed z-30"
+      <div className="flex justify-center items-center">
+        <button
+          onClick={() => setHandleClick(!handleClick)}
+          className="btnFilter z-50"
         >
+          {handleClick
+            ? "Cliquez ici pour fermer le filtre"
+            : "Cliquez ici pour afficher le filtre"}
+        </button>
+      </div>
+      <div className="cadre flex relative">
+        {handleClick && (
+          <section className="filter py-4 left-3 top-32 w-72 fixed z-30">
+            <div className="col-1">
+              <div className="row-1 mx-6 my-2">
+                <li className="flex flex-col">
+                  <label htmlFor="superficie">Superficies</label>
+                  <p className="flex text-sm justify-between">
+                    <span>{minSuperficie}</span>
+                    <span>{maxSuperficie}</span>
+                  </p>
+                  <input
+                    className="cursor-pointer"
+                    type="range"
+                    defaultValue={superficie}
+                    onChange={(e) => setSuperficie(e.target.value)}
+                    min="0"
+                    max="17098300"
+                    step="100"
+                  />
+                </li>
+                <p className="text-center text-sm">{superficie}</p>
+              </div>
+              <div className="row-2 mx-6 my-2">
+                <li className="flex flex-col">
+                  <label htmlFor="population">Populations</label>
+                  <p className="flex text-sm justify-between">
+                    <span>{minPopulation}</span>
+                    <span>{maxPopulation}</span>
+                  </p>
+                  <input
+                    className="cursor-pointer"
+                    type="range"
+                    defaultValue={populations}
+                    onChange={(e) => setPopulations(e.target.value)}
+                    min="0"
+                    max="1425893465"
+                    step="100"
+                  />
+                </li>
+                <p className="text-center text-sm">{populations}</p>
+              </div>
+              <div className="row-3 text-center my-4">
+                <input
+                  className="rounded-2xl py-2 pl-4 border-input"
+                  type="search"
+                  placeholder="Trouver un pays"
+                  value={sorted}
+                  onChange={(e) => setSorted(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="col-2 flex flex-col gap-5">
+              <div className="row-4 mb-20 select text-center w-56">
+                <div
+                  onClick={() => setToogle(!toogle)}
+                  className="select-btn bg-gray-300 rounded-xl p-2 flex justify-between cursor-pointer"
+                >
+                  <span>Continents</span>
+                  <img
+                    className="arrowdown w-4 h-8 -mt-2"
+                    src={ArrowDown}
+                    alt="arrowDown"
+                  />
+                </div>
+                {toogle && (
+                  <div className="option mt-1 bg-gray-300 rounded-lg">
+                    <div>
+                      <input
+                        type="search"
+                        value={searchContinent}
+                        onChange={(e) => setSearchContinent(e.target.value)}
+                        placeholder="Trouver un continent"
+                        className="bg-gray-300 mt-4 w-52 h-7 p-2 text-black rounded-lg border-input"
+                      />
+                    </div>
+                    <div className="flex flex-col mt-2 max-h-24 overflow-y-auto">
+                      {continents
+                        .filter((continent) =>
+                          continent
+                            .toLowerCase()
+                            .includes(searchContinent.toLowerCase())
+                        )
+                        .map((continent) => (
+                          <div
+                            className="option text-start pl-4 cursor-pointer"
+                            key={continent}
+                          >
+                            <li
+                              id={continent}
+                              onClick={(e) => setFilter(e.target.id)}
+                              className="p-1"
+                            >
+                              {continent}
+                            </li>
+                          </div>
+                        ))}
+                    </div>
+                  </div>
+                )}
+              </div>
+              <div className="row-5 select text-center w-56">
+                <div
+                  onClick={() => {
+                    setToogleLang(!toogleLang);
+                  }}
+                  className="select-btn bg-gray-300 rounded-xl p-2 flex justify-between cursor-pointer"
+                >
+                  <span>Langues</span>
+                  <img
+                    className="arrowdown w-4 h-8 -mt-2"
+                    src={ArrowDown}
+                    alt="arrowDown"
+                  />
+                </div>
+                {toogleLang && (
+                  <div className="option mt-1 bg-gray-300 rounded-lg">
+                    <div>
+                      <input
+                        type="search"
+                        value={searchLangue}
+                        onChange={(e) => setSearchLangue(e.target.value)}
+                        placeholder="Trouver une langue"
+                        className="bg-gray-300 mt-4 w-52 h-7 p-2 text-black rounded-lg border-input"
+                      />
+                    </div>
+                    <div className="flex flex-col mt-2 max-h-32 overflow-y-auto">
+                      {languages
+                        .filter((language) =>
+                          language
+                            .toLowerCase()
+                            .includes(searchLangue.toLowerCase())
+                        )
+                        .map((language) => (
+                          <div
+                            className="option text-start pl-4 cursor-pointer"
+                            key={language}
+                          >
+                            <li
+                              id={language}
+                              onClick={(e) => setLanguage(e.target.id)}
+                              className="p-1"
+                            >
+                              {language}
+                            </li>
+                          </div>
+                        ))}
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+          </section>
+        )}
+        <section className="filter none py-4 left-3 top-32 w-72 fixed z-30">
           <div className="col-1">
             <div className="row-1 mx-6 my-2">
               <li className="flex flex-col">
@@ -311,7 +446,6 @@ const Query = () => {
             </div>
           </div>
         </section>
-
         <ul className="pays px-10 color absolute left-72 top-32 z-0">
           {filteredCountries
             .filter((country) =>
